78f993975e0f3fb535ca8ae35e3fca9b1f4da60c
zhuwh9 <15347556575@163.com>
2018-06-29 23:56:22 +0800

    暂时移除错误URL检测

--- Codes/server/routers/auth_router.js
+++ Codes/server/routers/auth_router.js
@@ -65,7 +65,10 @@ module.exports = function(app) {
 							end restaurant's behaviors
 	***************************************************************************/
 
-	app.use((req, res, next) => {
-		res.status(404).json({"error":"request url not found"});
-	});
+	/**************************************************************************
+							Prevent Wrong Url request
+	**************************************************************************/
+	// app.use((req, res, next) => {
+	// 	res.status(404).json({"error":"request url not found"});
+	// });
 }
